298abe623c35315e7e00bc64ca6cf00fa82f6c05
Pius Uzamere <pius+git@alum.mit.edu>
2009-04-02 12:33:07 -0400

    tweaked query_param to work with Youtube (perhaps was just broken on Ruby 1.9?)

--- lib/acts_as_unvlogable/string_extend.rb
+++ lib/acts_as_unvlogable/string_extend.rb
@@ -3,6 +3,14 @@ class String
   def query_param(param_name)
     raise ArgumentError.new("param name can't be nil") if param_name.blank?
     uri = URI.parse(self)
-    (CGI::parse(uri.query)[param_name].to_s if uri.query) || nil
+    foo = uri.query ? CGI::parse(uri.query)[param_name] : nil
+    case foo
+    when Array
+      p = foo.first.to_s
+    when String
+      p = foo
+    else
+      p = foo ? foo.to_s : nil
+    end
   end
 end
